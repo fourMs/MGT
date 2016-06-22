d3768f20aef4b889f7fa1beb183c6a9622024276
benlyyan <bozhou@ifi.uio.no>
2016-06-22 17:32:24 +0800

    Update mgautocor.m

--- source-code/mgautocor.m
+++ source-code/mgautocor.m
@@ -4,13 +4,13 @@ function [per,ac,eac,lag] = mgautocor(mg,type,maxp,method)
 % period,autocorrelation functions and enhanced autocorrelation functions
 % and lags
 % syntax:[per,ac,eac,lag] = mgautocor(out,type,maxp,method);
-
+%
 % input:
 % mg: musical gestures data structure
 % maxp: maximum period ,default value,2
 % method: 'first','highest' maximal value of the autocorrelation function
 % is taken as periodicity estimation
-
+%
 % output:
 % per: a scalar containing the period estimation of the quantity of motion
 % ac: a vector containing the autocorrelation functions of quantity of
@@ -18,7 +18,7 @@ function [per,ac,eac,lag] = mgautocor(mg,type,maxp,method)
 % eac: a vector containing the enhanced autocorrelation functions of
 % quantity of motion
 % lag: a vector containing the lag values for the autocorrelation functions
-
+%
 % eg:[per,ac,eac,lag] = mgautocor(mg);
 if nargin == 2
     maxp = 2;
@@ -32,7 +32,7 @@ if nargin == 3
         method = 'first';
     end
 end
-if strcmp(type,'video')
+if strcmpi(type,'video')
 mg.video.qom = detrend(mg.video.qom);
 frate = mg.video.obj.FrameRate;
 maxlag = round(maxp*frate);
@@ -46,7 +46,7 @@ tmp1 = tmp1/max(tmp1);
 ind = find(diff(sign(diff(tmp1)))==-2);
 if isempty(ind)
     per = NaN;
-elseif strcmp(method,'highest')
+elseif strcmpi(method,'highest')
     w = min(ind);
     [q,ww] = max(tmp1(w:end));
     ma = w + ww - 1;
@@ -64,7 +64,7 @@ end
 tmp2 = max(tmp1,0);
 tmp3 = tmp2-interp1(lagfr,tmp2,lagfr/2);
 eac = max(tmp3,0);
-elseif strcmp(type,'mocap') && isfield(mg,'mocap')
+elseif strcmpi(type,'mocap') && isfield(mg,'mocap')
     mocap = mg.mocap;
     [m,n] = size(mocap.data);
     for i =  1:m-1
