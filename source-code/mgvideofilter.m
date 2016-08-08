2e2b24655fff7d43b7e99cf6355c64e190f408b8
benlyyan <bozhou@ifi.uio.no>
2016-08-08 16:32:56 +0800

    Update mgvideofilter.m

--- source-code/mgvideofilter.m
+++ source-code/mgvideofilter.m
@@ -1,18 +1,20 @@
 function mg = mgvideofilter(varargin)
-
 % mg = mgvideofilter(varargin) 
-% mgvideofilter filter the video with 'Temporal' ,'Spatial', 'Both'
+% mgvideofilter filter the video with 'Spatial' option. The filtered
+% video will be written back to disk with the name of 'original name+filtervideo.avi'
 % options.
-% with 'Temporal' option, perform temporal filter
 % with 'Spatial' option, perform spatial filter by mediean, or average filter; 
+%
 % syntax: mg = mgvideofilter(filename,type,med,h);
 % mg = mgvideofilter(mg,type,med,h);
-
-% input: 
+%
+%input: 
 % filename: video file
 % mg: input mg containing the video data;
 % type: 'Spatial','Temporal';
-% med: when 'Spatial',it has two options, 'Mediean', 'Average'; when 'Temporal'
+% med: when 'Spatial',it has two options, 'Mediean', 'Average';
+% h: element structure
+% 
 % output: 
 % mg: filtered video
 if isempty(varargin)
@@ -30,7 +32,7 @@ elseif l >=4
 end
 if ischar(varargin{1})
     file = varargin{1};
-    [~,~,ex] = fileparts(file);
+    [~,pr,ex] = fileparts(file);
     if ismember(ex,{'.mp4';'.avi';'mpg';'mov';'m4v'})
         mg = mgvideoreader(file);
     else 
@@ -40,9 +42,10 @@ elseif isstruct(varargin{1}) && isfield(varargin{1},'video')
     mg = varargin{1};
 end
 numf = floor(mg.video.obj.FrameRate*mg.video.obj.Duration);
-if strcmp(type,'Spatial')
-    if strcmp(med,'Mediean')
-        v = VideoWriter('videofiltered.avi');
+newfile = strcat(pr,'filtervideo.avi');
+if strcmpi(type,'Spatial')
+    if strcmpi(med,'Mediean')
+        v = VideoWriter(newfile);
         v.FrameRate = mg.video.obj.FrameRate;
         open(v);
         i = 1;
@@ -56,10 +59,12 @@ if strcmp(type,'Spatial')
             i = i + 1;
         end
         close(v);
-        s = mgvideoreader('videofiltered.avi');
+        s = mgvideoreader(newfile);
         mg.video.obj = s.video.obj;
-    elseif strcmp(med,'Average')
-        v = VideoWriter('videofiltered.avi');
+        mg.type = 'mg data';
+        mg.createtime = datestr(datetime('today'));
+    elseif strcmpi(med,'Average')
+        v = VideoWriter(newfile);
         v.FrameRate = mg.video.obj.FrameRate;
         open(v);
         i = 1;
@@ -72,15 +77,12 @@ if strcmp(type,'Spatial')
             i = i + 1;
         end
         close(v);
-        s = mgvideoreader('videofiltered.avi');
+        s = mgvideoreader(newfile);
         mg.video.obj = s.video.obj;
-        
+        mg.type = 'mg data';
+        mg.createtime = datestr(datetime('today'));
     end
-end
-mg.type = 'mg data';
-mg.createtime = datestr(datetime('today'));
-    
-        
+end     
         
 
 
