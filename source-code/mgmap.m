44c920dc21acaf6b2d105ee26a3316325e62cd21
benlyyan <bozhou@ifi.uio.no>
2016-08-08 15:57:42 +0800

    Update mgmap.m

--- source-code/mgmap.m
+++ source-code/mgmap.m
@@ -2,20 +2,22 @@ function mg = mgmap(varargin)
 % function mg = mgcrop(varargin)
 % mgmap extract the same segment of audio, mocap data
 % according to video, if the filename of audio is not given, then mgmap trys to 
-% read audio data from video.
-
+% read audio data from video.In this case, the input mg must contain the
+% originl video information such that the corresponding audio can be
+% extracted. e.g out of mgvideoreader.
+%
 % syntax: mg = mgmap(mg,'Audio');
 % mg = mgmap(mg,'Audio',filename);
 % mg = mgmap(mg,'Mocap',filename);
 % mg = mgmap(mg,'Both',filename1,filename2);
 % mg = mgmap(mg,'Both');
-
+%
 % input:
 % mg: musical gestures data sturcture containing the video data and
 % parameters
 % 'Audio','Mocap','Both': indicate the mapping type
 % filename1,filename2: names of audio file and mocap file
-
+%
 % output:
 % mg: musical gestures data sturcture containing mapped data
 
@@ -23,17 +25,17 @@ mg = varargin{1};
 filename = mg.video.obj.Name;
 starttime = mg.video.orig_starttime;
 endtime = mg.video.orig_endtime;
-if strcmp(varargin{2},'Audio')
+if strcmpi(varargin{2},'Audio')
     if nargin > 2
         filename = varargin{3};
         mg.audio.mir = miraudio(filename,'Extract',starttime,endtime);
     else
         mg.audio.mir = miraudio(filename,'Extract',starttime,endtime);
     end
-elseif length(varargin)>2 && strcmp(varargin{2},'Mocap')
+elseif length(varargin)>2 && strcmpi(varargin{2},'Mocap')
     mg.mocap = mcread(varargin{3});
     mg.mocap = mctrim(mg.mocap,starttime,endtime);
-elseif length(varargin)> 2 && strcmp(varargin{2},'Both')
+elseif length(varargin)> 2 && strcmpi(varargin{2},'Both')
     if nargin >3
         filename = varargin{3};
         mg.audio.mir = miraudio(filename,'Extract',starttime,endtime);
@@ -44,7 +46,7 @@ elseif length(varargin)> 2 && strcmp(varargin{2},'Both')
         mg.mocap = mcread(varargin{3});
         mg.mocap = mctrim(mg.mocap,starttime,endtime);
     end
-elseif length(varargin) == 2 && strcmp(varargin{2},'Both')
+elseif length(varargin) == 2 && strcmpi(varargin{2},'Both')
     mg.audio.mir = miraudio(mg.audio.mir,'Extract',starttime,endtime);
     mg.mocap = mctrim(mg.mocap,starttime,endtime);
 end
