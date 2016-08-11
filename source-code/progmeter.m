5d62430f4b8a9189ec61061c63dff98088ae5353
benlyyan <bozhou@ifi.uio.no>
2016-08-11 17:50:47 +0800

    Add files via upload

---
+++ source-code/progmeter.m
@@ -0,0 +1,22 @@
+function progmeter(i,n,w)
+
+if nargin<3, 
+    w = 1;
+end
+if i==1    
+    fwrite(1,sprintf('process in progress:  \n'));
+    return;
+elseif ischar(i)
+    fwrite(1,sprintf('%s\n',i));
+    fwrite(1,sprintf('00%%'));
+    return;
+end
+
+if mod(i,w*n/100) <= mod(i-1,w*n/100),
+    fwrite(1,sprintf('\b\b\b'));
+    fwrite(1,sprintf('%2d%%', round(100*i/n)));
+end
+
+if i==n,
+  fprintf(1,'\n');
+end
