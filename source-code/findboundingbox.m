1798848f300201e79146015016ebda4764e5c81f
benlyyan <bozhou@ifi.uio.no>
2016-08-15 15:38:00 +0800

    helpful function

---
+++ source-code/findboundingbox.m
@@ -0,0 +1,21 @@
+function [bbox,aom] = findboundingbox(f)
+% bbox = findboundingbox(f) 
+% find the boundingbox and aom of the image f
+if size(f,3) == 3
+    f = rgb2gray(f);
+end
+level = multithresh(f);
+bw = f > level;
+bw = medfilt2(bw,[3,3]);
+stats = regionprops('table',bw,'BoundingBox','Area');
+aom = sum(stats.Area);
+stats = stats.BoundingBox;
+bbox = min(stats);
+[row,col] = find(bw>0);
+bbox(3) = max(col) - min(col);
+bbox(4) = max(row) - min(row);
+
+
+
+
+
