454f27e8678a65b835fac39d392b59b7d912caf6
Alexander Refsum Jensenius <arjensenius@gmail.com>
2018-02-08 14:16:53 +0100

    New examples

--- example/examples.m
+++ example/examples.m
@@ -2,19 +2,20 @@
 % Some examples on usage of the Musical Gestures Toolbox for Matlab
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+fn='dance.avi';
 
 % Generate a motion video and motiongrams (both directions)
 % with the file endings _motion.avi and _mgx.tiff/_mgy.tiff
-mgmotion('pianist.mp4');
+mgmotion(fn);
 
 % Generate an optical flow video
 % with the file ending _flow.avi
-mgmotion('pianist.mp4','OpticalFlow');
+mgmotion(fn,'OpticalFlow');
 
 % Generate a motion history video
 % with the ending _history.avi
-mgmotionhistory('pianist.mp4');
+mgmotionhistory(fn);
 
 % Generate a motion average image
 % with the ending _average.tiff
-mgmotionaverage('pianist.mp4');
+mgmotionaverage(fn);
