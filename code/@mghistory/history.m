0e6bf15f7d7a36f259b9aedffb7a333f3aeb3f34
Habibur Rahman <hr19392@gmail.com>
2020-05-04 21:04:43 +0200

    completed class for mgaverage

--- code/@mghistory/history.m
+++ code/@mghistory/history.m
@@ -3,52 +3,38 @@ function [retval,videoOut] = history(obj, varargin)
 %   Detailed explanation goes here
 
 
-cmd = [];
-cmd.nFrame = 20; %default fame number = 20;
-cmd.color = 'off';
-cmd.frameInterval = 1;
-cmd.fileCount = length(obj.video);
-cmd.startTime = [];
-cmd.stopTime = [];
-cmd.fileCount = length(obj.video);
+arg = [];
+arg.nFrame = 20; %default fame number = 20;
+arg.color = 'off';
+arg.frameInterval = 1;
+arg.fileCount = length(obj.video);
+arg.startTime = [];
+arg.stopTime = [];
+arg.fileCount = length(obj.video);
 
 
 
-if(cmd.fileCount <= 1)
-    cmd.startTime = 0;
-    cmd.stopTime = obj.video.Duration;
-else
-    
-    
+if(arg.fileCount <= 1)
+    arg.startTime = obj.startTime;
+    arg.stopTime = obj.stopTime;
 end
-
 
 
 for argi = 1:nargin-1
     
     if( ischar(varargin{argi}))
-        if (strcmpi(varargin{argi},'startTime'))
-            if(cmd.fileCount <= 1)
-                disp('start time specified');
-                cmd.startTime = varargin{argi+1};
-            end
-        elseif (strcmpi(varargin{argi},'stopTime'))
-            if(cmd.fileCount <= 1)
-                disp('stop time specified');
-                cmd.stopTime = varargin{argi+1};
-            end
-        elseif (strcmpi(varargin{argi},'nFrame'))
+        if (strcmpi(varargin{argi},'nFrame'))
             disp('nframe specified');
-            cmd.nFrame = varargin{argi+1};
+            arg.nFrame = varargin{argi+1};
         elseif (strcmpi(varargin{argi},'Color'))
             disp('color mode on is specified in argument');
-            cmd.color = 'on'
+            arg.color = 'on'
         elseif (strcmpi(varargin{argi},'Gray'))
             disp('color mode on is specified in argument');
-            cmd.color = 'off'
+            arg.color = 'off'
         elseif (strcmpi(varargin{argi},'Interval'))
             if(argi + 1 <= nargin &&  isnumeric(varargin{argi + 1}))
-                cmd.frameInterval = varargin{argi+1};
+                arg.frameInterval = varargin{argi+1};
             end
         end
     end
@@ -57,7 +43,7 @@ end
 
 
 
-if(cmd.fileCount <= 1)
+if(arg.fileCount <= 1)
     
     [~,pr,ex] = fileparts(obj.video.Name);
     newfile = strcat(pr,'_history.avi');
@@ -65,15 +51,15 @@ if(cmd.fileCount <= 1)
     v.FrameRate = obj.video.FrameRate;
     open(v);
     
-    obj.video.CurrentTime = cmd.startTime;
+    obj.video.CurrentTime = arg.startTime;
     
     
-    if(strcmpi(cmd.color, 'on'))
-        temparray = zeros(obj.video.Height,obj.video.Width,3,cmd.nFrame-1,'uint8');
+    if(strcmpi(arg.color, 'on'))
+        temparray = zeros(obj.video.Height,obj.video.Width,3,arg.nFrame-1,'uint8');
         prevFrame = readFrame(obj.video);
-        numfr = obj.video.FrameRate*(cmd.stopTime-cmd.startTime)-cmd.nFrame;
+        numfr = obj.video.FrameRate*(arg.stopTime-arg.startTime)-arg.nFrame;
         progmeter(0);
-        for indf = 1:cmd.frameInterval:numfr-1
+        for indf = 1:arg.frameInterval:numfr-1
             progmeter(indf,numfr)
             temparray = temparray(:,:,:,[2:end 1]);
             currentFrame = readFrame(obj.video);
@@ -82,16 +68,16 @@ if(cmd.fileCount <= 1)
             temparray(:,:,:,end) = temp;
             history = uint8(sum(temparray,4));
             writeVideo(v,imadd(history,currentFrame));
-            obj.video.CurrentTime = cmd.startTime + (1/obj.video.FrameRate)*indf;
+            obj.video.CurrentTime = arg.startTime + (1/obj.video.FrameRate)*indf;
             
         end
         
     else
-        temparray = zeros(obj.video.Height,obj.video.Width,cmd.nFrame-1,'uint8');
+        temparray = zeros(obj.video.Height,obj.video.Width,arg.nFrame-1,'uint8');
         prevFrame = rgb2gray(readFrame(obj.video));
-        numfr = obj.video.FrameRate*(cmd.stopTime-cmd.startTime)-cmd.nFrame;
+        numfr = obj.video.FrameRate*(arg.stopTime-arg.startTime)-arg.nFrame;
         progmeter(0);
-        for indf = 1:cmd.frameInterval:numfr-1
+        for indf = 1:arg.frameInterval:numfr-1
             progmeter(indf,numfr)
             temparray = temparray(:,:,[2:end 1]);
             currentFrame = rgb2gray(readFrame(obj.video));
@@ -100,7 +86,7 @@ if(cmd.fileCount <= 1)
             temparray(:,:,end) = temp;
             history = uint8(sum(temparray,3));
             writeVideo(v,imadd(history,currentFrame));
-            obj.video.CurrentTime = cmd.startTime + (1/obj.video.FrameRate)*indf;
+            obj.video.CurrentTime = arg.startTime + (1/obj.video.FrameRate)*indf;
             
         end
     end
@@ -115,7 +101,7 @@ if(cmd.fileCount <= 1)
 else
     
     
-    for i=1:cmd.fileCount
+    for i=1:arg.fileCount
         [~,pr,ex] = fileparts(obj.video{i}.Name);
         newfile = strcat(pr,'_history.avi');
         v = VideoWriter(newfile);
@@ -123,15 +109,15 @@ else
         open(v);
         
         obj.video{i}.CurrentTime = 0;
-        cmd.startTime = 0;
-        cmd.stopTime = obj.video{i}.Duration;
+        arg.startTime = 0;
+        arg.stopTime = obj.video{i}.Duration;
         
-        if(strcmpi(cmd.color, 'on'))
-            temparray = zeros(obj.video{i}.Height,obj.video{i}.Width,3,cmd.nFrame-1,'uint8');
+        if(strcmpi(arg.color, 'on'))
+            temparray = zeros(obj.video{i}.Height,obj.video{i}.Width,3,arg.nFrame-1,'uint8');
             prevFrame = readFrame(obj.video{i});
-            numfr = obj.video{i}.FrameRate*(cmd.stopTime-cmd.startTime)-cmd.nFrame;
+            numfr = obj.video{i}.FrameRate*(arg.stopTime-arg.startTime)-arg.nFrame;
             progmeter(0);
-            for indf = 1:cmd.frameInterval:numfr-1
+            for indf = 1:arg.frameInterval:numfr-1
                 progmeter(indf,numfr)
                 temparray = temparray(:,:,:,[2:end 1]);
                 currentFrame = readFrame(obj.video{i});
@@ -140,16 +126,16 @@ else
                 temparray(:,:,:,end) = temp;
                 history = uint8(sum(temparray,4));
                 writeVideo(v,imadd(history,currentFrame));
-                obj.video{i}.CurrentTime = cmd.startTime + (1/obj.video{i}.FrameRate)*indf;
+                obj.video{i}.CurrentTime = arg.startTime + (1/obj.video{i}.FrameRate)*indf;
                 
             end
         else
             
-            temparray = zeros(obj.video{i}.Height,obj.video{i}.Width,cmd.nFrame-1,'uint8');
+            temparray = zeros(obj.video{i}.Height,obj.video{i}.Width,arg.nFrame-1,'uint8');
             prevFrame = rgb2gray(readFrame(obj.video{i}));
-            numfr = obj.video{i}.FrameRate*(cmd.stopTime-cmd.startTime)-cmd.nFrame;
+            numfr = obj.video{i}.FrameRate*(arg.stopTime-arg.startTime)-arg.nFrame;
             progmeter(0);
-            for indf = 1:cmd.frameInterval:numfr-1
+            for indf = 1:arg.frameInterval:numfr-1
                 progmeter(indf,numfr)
                 temparray = temparray(:,:,[2:end 1]);
                 currentFrame = rgb2gray(readFrame(obj.video{i}));
@@ -158,7 +144,7 @@ else
                 temparray(:,:,end) = temp;
                 history = uint8(sum(temparray,3));
                 writeVideo(v,imadd(history,currentFrame));
-                obj.video{i}.CurrentTime = cmd.startTime + (1/obj.video{i}.FrameRate)*indf;
+                obj.video{i}.CurrentTime = arg.startTime + (1/obj.video{i}.FrameRate)*indf;
                 
             end
         end
