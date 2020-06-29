a42fe9f937918412c9a5b54c10208ac40f63e3a2
Habibur Rahman <hr19392@gmail.com>
2020-06-29 18:02:11 +0200

    completed class for mgflow

--- code/@mgflow/flow.m
+++ code/@mgflow/flow.m
@@ -67,17 +67,20 @@ else
 end
 
 
-obj.mg.video.gram.y = [];
-obj.mg.video.gram.x = [];
-obj.mg.video.qom = [];
-obj.mg.video.com = [];
-obj.mg.video.aom = [];
-obj.mg.video.wom = [];
-obj.mg.video.hom = [];
 
 
 
 if(arg.fileCount <= 1)
+    
+    obj.mg.video.gram.y = [];
+    obj.mg.video.gram.x = [];
+    obj.mg.video.qom = [];
+    obj.mg.video.com = [];
+    obj.mg.video.aom = [];
+    obj.mg.video.wom = [];
+    obj.mg.video.hom = [];
+
+    
     [filepath,filename,ext] = fileparts(obj.video.Name);
     newfile = strcat(filename,'_flow.avi');
     v = VideoWriter(newfile);
@@ -131,10 +134,64 @@ if(arg.fileCount <= 1)
     
 else
     for fileIndex = 1:arg.fileCount
+        obj.mg{fileIndex}.video.gram.y = [];
+        obj.mg{fileIndex}.video.gram.x = [];
+        obj.mg{fileIndex}.video.qom = [];
+        obj.mg{fileIndex}.video.com = [];
+        obj.mg{fileIndex}.video.aom = [];
+        obj.mg{fileIndex}.video.wom = [];
+        obj.mg{fileIndex}.video.hom = [];
         
         
-        
-        
+        [filepath,filename,ext] = fileparts(obj.video{fileIndex}.Name);
+        newfile = strcat(filename,'_flow.avi');
+        v = VideoWriter(newfile);
+        v.FrameRate = obj.video{fileIndex}.FrameRate;
+        open(v);
+
+        fh = figure('visible','off');
+        fr1 = rgb2gray(readFrame(obj.video{fileIndex}));
+
+        obj.video{fileIndex}.CurrentTime = obj.startTime{fileIndex};
+        numfr = obj.video{fileIndex}.FrameRate*(obj.stopTime{fileIndex}-obj.startTime{fileIndex});
+        %obj.video.
+        ii = 0;
+        for i = 1:arg.frameInterval:numfr-1
+
+
+            fr2 = rgb2gray(readFrame(obj.video{fileIndex}));
+            flow = mgopticalflow(fr2,fr1);
+            magnitude = flow.Magnitude;
+            if filterflag
+                magnitude = mgmotionfilter(magnitude,filtertype,thresh);
+            end
+            qom = sum(sum(magnitude));
+            [m,n] = size(magnitude);
+            x = 1:n;
+            y = 1:m;
+            meanx = mean(magnitude);
+            comx = x*meanx'/sum(meanx);
+            meany = mean(magnitude,2);
+            comy = y*meany/sum(meany);
+            com = [comx,comy];
+            gramx = mean(magnitude);
+            gramy = mean(magnitude,2);
+
+            obj.mg{fileIndex}.video.gram.y = [obj.mg{fileIndex}.video.gram.y;gramx];
+            obj.mg{fileIndex}.video.gram.x = [obj.mg{fileIndex}.video.gram.x,gramy];
+            obj.mg{fileIndex}.video.qom = [obj.mg{fileIndex}.video.qom;qom];
+            obj.mg{fileIndex}.video.com = [obj.mg{fileIndex}.video.com;com];
+            imshow(fr1),hold on;
+            plot(flow,'DecimationFactor',[20 20],'ScaleFactor',10);
+            hold off;
+            writeVideo(v,getframe(fh));
+            fr1 = fr2;
+            progmeter(obj.video{fileIndex}.CurrentTime,obj.stopTime{fileIndex});
+
+            obj.video{fileIndex}.CurrentTime = obj.startTime{fileIndex} + (1/obj.video{fileIndex}.FrameRate)*i;
+        end
+        close(v);
+        disp(['The motion video is created with name ',newfile]);
         
     end
 end
